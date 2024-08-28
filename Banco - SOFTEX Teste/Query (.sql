38c4c296832de8c8a6d16935ce3ec942bf656281
Rikelmy Barboza Feitosa <132027730+RikelmyBarboza@users.noreply.github.com>
2024-08-27 21:52:47 -0300

    Add files via upload

---
+++ Banco - SOFTEX Teste/Query (.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS banco;
+
+CREATE TABLE IF NOT EXISTS dados (
+    id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    data_criacao DATE,
+    saldo INT
+
+);
+
+CREATE TABLE IF NOT EXISTS extrato (
+    id INT,
+    nome VARCHAR(255),
+    data_criacao DATE,
+    saldo INT
+
+);
+
